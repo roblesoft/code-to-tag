0a5783106437a55aa3db40e90c11c17c9d3155c7
Uriel R <uriel_r@outlook.com>
2017-11-04 01:58:22 -0600

    se arreglaron bugs

--- tagen.py
+++ tagen.py
@@ -1,46 +1,59 @@
 code = open('code.txt', 'w')
 all_letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-limits = '()[]{}.;1234567890'
-signs = '-+*/='
+limits = '()[]{}.,;1234567890'
+signs = '-+*/=<>'
 ind = 0
-caracter = '() =.\t[]{}'
-reserver_words = ['for', 'if', 'import', 'while', 'do', 'JOptionpane', 'else', 'int', 'String', 'float', 'public', 'static', 'void', 'class']
-code.write("<style>.tab{color: #222526} .reserver{color: #FC4349 } .limits{color:#FFF} .var{color:#B2FF55} .func{color:#00E4ED} .signs{color:#FC4349} .string{color:#EAFF11} .codigo{background-color: #222526; padding: 10%; border-radius: 10px} .ind{color:grey}</style>\n")
+caracter = '() =.\t[]{};+-,'
+ends = '() = \t{};-+'
+reserver_words = ['for', 'if', 'new', 'import', 'while', 'do', 'JOptionpane', 'else', 'int', 'String', 'float', 'public', 'static', 'void', 'class', 'null']
+code.write("<style>.tab{color: #222526} .reserver{color: #FC4349 } .limits{color:#FFF} .var{color:#ADD96C} .func{color:#00E4ED} .signs{color:#FC4349} .string{color:#EAFF11} .codigo{background-color: #222526; padding: 10%; border-radius: 10px} .ind{color:grey}</style>\n")
+code.write("<!--code by roblesoft, github.com/roblesoft-->")
 tag = "<code>\n"
 code.write(tag)
 tag = '\t<div class="codigo">\n'
 code.write(tag)
+rel = False
 for line in open('input.txt'):
     end = 0
     line_of_code = line.replace("\n","")
     char = line_of_code
     ind += 1
-    indi = '\t\t<span class="ind">{}</span>\n'. format(ind)
+    indi = '\t\t<span class="ind">{}</span>'. format(ind)
     code.write(indi)
+    word = '---'
+    tag = "<span class ='tab'>{}</span>". format(word)
+    code.write(tag)
     for j in range(len(line_of_code)):        
         if char[j] == '\t':
             word = "----"
-            tag = "\t\t<span class ='tab'>{}</span>\n". format(word)
+            tag = "<span class ='tab'>{}</span>". format(word)
             code.write(tag)
-        elif char[j] in limits:
+        elif char[j] in limits and rel == False:
             word = char[j: j+1]
-            tag = "\t\t<span class ='limits'>{}</span>\n". format(word)
+            tag = "<span class ='limits'>{}</span>". format(word)
             code.write(tag)
-        elif char[j] in signs:
+        elif char[j] == ' ' and rel == False:
+            word = '-'
+            tag = "<span class ='tab'>{}</span>". format(word)
+            code.write(tag)
+        elif char[j] in signs and rel == False:
             word = char[j: j+1]
-            tag = "\t\t<span class ='signs'>{}</span>\n". format(word)
+            tag = "<span class ='signs'>{}</span>". format(word)
             code.write(tag)
         elif char[j] == '"':
             gat = 0
+            rel = True
+            if char[j+1] == ';' or char[j+1] == ')' or char[j+1] == '+' or char[j+1] == ' ':
+                rel = False
+                continue
             for w in range(j, len(line_of_code)):
-                if char[w] == '"':
+                if char[w] == '"' and char[w + 1] in ends:
                     word = line_of_code[j:j+gat+1]
-                    tag = "\t\t<span class ='string'>{}</span>\n". format(word)
+                    tag = "<span class ='string'>{}</span>". format(word)
                     code.write(tag)
-                gat += 1
-            j += gat
-        
-        elif char[j] in all_letters:
+                    break
+                gat += 1        
+        elif char[j] in all_letters and rel == False:
             end = 0
             if char[j-1] in all_letters:
                 continue
@@ -49,20 +62,20 @@ for line in open('input.txt'):
                     string = line_of_code[j:j+end]
                     if string in reserver_words:
                         word = string
-                        tag = "\t\t<span class ='reserver'>{}</span>\n". format(word)
+                        tag = "<span class ='reserver'>{}</span>". format(word)
                     elif line_of_code[j+end] == '(':
                         word = string
-                        tag = "\t\t<span class ='func'>{}</span>\n". format(word)
+                        tag = "<span class ='func'>{}</span>". format(word)
                     else:
                         word = string
-                        tag = "\t\t<span class ='var'>{}</span>\n". format(word)
+                        tag = "<span class ='var'>{}</span>". format(word)
                     code.write(tag)
                     break
                 end += 1
         elif char[j] == ' ':
             continue
         if j + 1 == len(line_of_code):
-            tag = "\t\t<br>\n"
+            tag = "<br>"
             code.write(tag)
 tag = "\t</div>\n"
 code.write(tag)
